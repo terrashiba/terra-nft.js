1a7556908e090d2134579428283fb5109b299684
ALPAC-4 <contact.alpac4@gmail.com>
2021-09-24 14:01:40 +0900

    feat: add more comment

--- src/sample.ts
+++ src/sample.ts
@@ -3,7 +3,8 @@ import { CW721 } from 'contracts'
 import { Executor } from 'executor'
 import { getContractAddressFromInstantiateResult } from 'utils'
 
-// this is a sample code for bomboay-10
+// this is a sample code for bomboay-11
+// sample of instantiate, mint and transfer
 
 // test wallet address - terra1p204wtykwke52hcyt6vdh630725rdayczyzcvz
 // if there are no UST in the wallet get UST from https://faucet.terra.money/
@@ -23,6 +24,8 @@ async function main() {
   })
 
   // recommand to use fixed fee
+
+  // gas_limit: gas amount, amount: total fee amount. (calculation of amount: gas_price * gas_limit + tax)
   const fee = new Fee(500000, '75000uusd')
 
   const key = new MnemonicKey({mnemonic: MNEMONIC})
@@ -36,14 +39,14 @@ async function main() {
   // generate executor
   const executor = new Executor(key, lcd)
 
-  // CW721 contract
+  // CW721 class
   const cw721 = new CW721({
     codeID: cw721CodeId,
     lcd,
     key
   })
 
-  // instantiateCW721
+  // instantiate CW721 (make new cw721 contract)
   const initMsg = cw721.init({
     name: 'test nft',
     symbol: 'TNFT',
@@ -56,17 +59,22 @@ async function main() {
     sequence,
     accountNumber
   })
-  
-  sequence++
 
+  // need Tx result to get cw721 contract address 
   const initTxInfo = await executor.pollingTx(initBroadcastResult)
 
+  sequence++
+
+  // pull contract address from the raw log
   const nftContractAddress = getContractAddressFromInstantiateResult(initTxInfo)
 
+  // set address on cw721 class
   cw721.contractAddress = nftContractAddress
 
   console.log(`Generate NFT contract, contract address - ${nftContractAddress}`)
 
+
+  // mint NFT
   const mintMsg = cw721.mint(
     'NFT_1', // NFT id
     'NFT', // NFT name
@@ -84,13 +92,15 @@ async function main() {
 
   console.log(`Mint new NFT`)
 
+  // wait for polling, because without having own node you will get sequence problem.
   await executor.pollingTx(mint)
 
   sequence++
 
-  const transferMsg = cw721.transfer(
+  const transferMsg = cw721.send(
     'terra123xkp64gmwqxklgcf224a6r4pkmunggzpd420l',
-    'NFT_1'
+    'NFT_1',
+    JSON.parse('{}')
   )
 
   await executor.execute({
